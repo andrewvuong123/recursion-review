3a97991ed7f252ec178ba4d245467443329c9304
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-17 15:35:41 -0700

    Implemented stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  result = '';
+  if (Array.isArray(obj)) {
+    result += stringifyArray(obj);
+  } else if (typeof obj === 'object') {
+    result += stringifyObj(obj);
+  } else if (typeof obj === 'string') {
+    result += '"' + obj + '"';
+  } else {
+    result += obj;
+  }
+  return result;
+};
+
+// helper fcn array
+var stringifyArray = function(arr) {
+  var result = [];
+  if (arr.length === 0) {
+    return '[]';
+  }
+  for (let i = 0; i < arr.length; i++) {
+    result.push(stringifyJSON(arr[i]));
+  }
+  return '[' + result + ']';
+};
+// helper fcn objects
+var stringifyObj = function(obj) {
+  var result = [];
+  if (obj === null) {
+    return 'null';
+  } else if (obj === {}) {
+    return '{}';
+  } else {
+    for (let key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        return '{}';
+      }
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+  }
+  return '{' + result + '}';
 };
